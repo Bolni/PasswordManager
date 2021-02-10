a5e15bf370b9ed9e3b8e11422558e17b003a0de9
Balint Molnar <molnar.balint4@hotmail.com>
2021-02-10 21:35:13 +0100

    csv handling part1

--- main.py
+++ main.py
@@ -3,19 +3,23 @@
 
 import json
 import sys
+import csv
 
 #def readData(username, password):
     
-print("Enter the website name")
-for line in sys.stdin:
-    w_name = input()
-    #if(w_name == website name found in csv):
-        #print("Website already saved")
-        #break
-    #else:
-    print("Enter your username: ")
+print("Enter the website name: ")
+with open('password.csv', mode='w', newline = '') as csvfile:
+    fieldnames = ['website_name' , 'username' , 'password']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     for line in sys.stdin:
-        username = input()
-    print("Enter your password: ")
-    for line in sys.stdin:
-        password = input()
+        w_name = input()
+        #if(w_name == website name found in csv):
+            #print("Website already saved")
+            #break
+        #else:
+        print("Enter your username: ")
+        for line in sys.stdin:
+            username = input()
+        print("Enter your password: ")
+        for line in sys.stdin:
+            password = input()
