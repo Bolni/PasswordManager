a4db02c45138c5fe28616b83c374b3ace3f37a78
Balint Molnar <molnar.balint4@hotmail.com>
2021-02-11 19:45:25 +0100

    writing to csv part1

--- main.py
+++ main.py
@@ -4,22 +4,27 @@
 import json
 import sys
 import csv
+import pandas as pd
+from cryptography.fernet import Fernet
 
 #def readData(username, password):
     
 print("Enter the website name: ")
 with open('password.csv', mode='w', newline = '') as csvfile:
+    
     fieldnames = ['website_name' , 'username' , 'password']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    
     for line in sys.stdin:
         w_name = input()
-        #if(w_name == website name found in csv):
-            #print("Website already saved")
-            #break
-        #else:
+        writer.writerow('website_name' : w_name)
+        
         print("Enter your username: ")
         for line in sys.stdin:
             username = input()
+            writer.writerow('username' : username)
+        
         print("Enter your password: ")
         for line in sys.stdin:
             password = input()
+            writer.writerow('password' : password)
